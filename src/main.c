5fcf65907000d6707740daa9bdde174653690dca
Eric Gharakhanian <EricGhara@gmail.com>
2025-12-29 22:19:17 -0800

    return gauge pressure

--- src/main.c
+++ src/main.c
@@ -6,18 +6,38 @@
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 
-// This is LED pin for pico. It does NOT work for pico W
-const uint LEDPIN = 25;
+// This us the V_s voltage provided to MPXV6615V
+const double V_REF = 5.;
+// Multiple MPXV6615V V_OUT is scaled by via resistor divider
+// to allow 3V3 ADC to interface with ~5V sensor
+// Note: Max voltage ADC should see is REF_VOLTAGE * 0.92 (at atmospheric pressure)
+const double V_OUT_MULTIPLE = 2400./(1000+2400);
+
 const uint ADC_0 = 0;
 const uint ADC_1 = 1;
 
+// volts per bit in raw ADC measurement
 const float CONVERSION_FACTOR = 3.3f / (1 << 12);
 
-double convert_raw(uint accumulator, uint num_samples)
+/**
+ * returns gauge pressure in kPa i.e. delta from atmospheric (e.g. -101.3 would
+ * be perfect vacuum under standard atmospheric pressure)
+ * @param accumulator total of all samples (raw adc values)
+ * @param num_samples number of samples
+ * @return gauge pressure in kPa
+ */
+double convert_raw(uint** accumulator, uint num_samples)
 {
-    double raw_avg = ((double)accumulator) / num_samples;
-    double volts = raw_avg * CONVERSION_FACTOR;
-    return volts;
+    // voltage under 0 vacuum (i.e. atmospheric pressure)
+    const static double OFFSET = V_REF * 0.92;
+    // response kpa/v
+    const static double SCALE = V_REF * 0.007652;
+
+    double raw_avg = ((double)**accumulator) / num_samples;
+    double v_out = (raw_avg * CONVERSION_FACTOR) / V_OUT_MULTIPLE;
+    double vac = (v_out - OFFSET) / SCALE;
+    **accumulator = 0;
+    return vac;
 }
 
 int main() {
@@ -37,11 +57,11 @@ int main() {
         cnt++;
 
         if (cnt == 100) {
-            double volts_0 = convert_raw(adc_accumulators[0], cnt);
-            double volts_1 = convert_raw(adc_accumulators[1], cnt);
-            printf("%f, %f\n", volts_0, volts_1);
+            double pres_0 = convert_raw(&adc_accumulators, cnt);
+            uint* p_elem_1 = adc_accumulators+1;
+            double pres_1 = convert_raw(&p_elem_1, cnt);
+            printf("%f, %f\n", pres_0, pres_1);
             cnt = 0;
-            memset(adc_accumulators, 0, sizeof(uint) * 2);
         }
         sleep_ms(5);
     }
